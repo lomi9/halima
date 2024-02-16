46dec5f69b47ede190fbaff9e182f8894ab057a1
lomi9 <119067005+lomi9@users.noreply.github.com>
2024-02-16 22:51:22 +0100

    feat : products pages on mobile

--- src/app/components/ProductsBanner.jsx
+++ src/app/components/ProductsBanner.jsx
@@ -7,9 +7,9 @@ function ProductsBanner({ image, title, paragraph }) {
         <Image layout="fill" src={image} alt='Paysage marocain' className='w-full h-full object-cover'/>
         <div className='filter absolute w-full h-full bg-black opacity-30 z-20'>
         </div>
-        <div className='absolute content w-full sm:w-2/3 h-full p-20 z-30 flex flex-wrap justify-start content-center'>
-                <h1 className='relative kodchasan font-normal uppercase text-4xl text-main-color z-30'>{title}</h1>
-                <p className='relative chakra text-lg font-thin text-main-color z-30'>{paragraph}</p>
+        <div className='absolute content w-full lg:w-2/3 h-full p-10 pt-20 lg:p-20 lg:pt-28 z-30 flex flex-wrap justify-start content-center'>
+                <h1 className='relative kodchasan font-normal uppercase text-2xl sm:text-4xl text-main-color z-30'>{title}</h1>
+                <p className='relative chakra text-base sm:text-lg lg:text-2xl font-thin text-main-color z-30'>{paragraph}</p>
 
             </div>
     </div>
